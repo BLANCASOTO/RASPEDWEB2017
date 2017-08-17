e8e1eebdb424de7e49a98aa8ec597f00c305a9dd
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-17 17:21:19 -0500

    Create mysql-login.php

---
+++ content/mysql-login.php
@@ -0,0 +1,7 @@
+<?php
+  //Datos de conexión a la base de datos
+  $server = "bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+  $user = "jiwornjvhyvd1y91";
+  $pass = "w8o8wmj01z4hw4pz";
+  $bd = "wkwxn90mtdsft056";
+?>
