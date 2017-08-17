44f35b119ea96de331aeaa72d0e29e3b402336d9
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-17 17:11:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-	<title>GPS Warning - BM</title>
-	<link rel="icon" type="image/ico" href="images/logo.ico" />
+	<title>RASPED</title>
+	<link rel="icon" type="image/png" href="https://image.ibb.co/hksXN5/RASPED.png" />
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/bootstrap_logg.css" rel="stylesheet">		
 </head>
