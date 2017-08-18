2380c52db3e3be9ddefed8d516ab02aff5856803
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-18 10:12:46 -0500

    Update evalua_log.php

--- content/evalua_log.php
+++ content/evalua_log.php
@@ -12,13 +12,33 @@ $telefono = $_REQUEST['telefono'];
 //encriptar contrasena
 $contrasena_ing = md5($contrasena_ing);
 $contrasena_db = "null";
+
+$id_personal = "null";
+$sede = "null";
 $cupo = "null";
+$nombre_personal = "null";
+$apellido_p = "null";
+$apellido_m = "null";
+$lada = "null";
+$telefono = "null";
+$contrasena = "null";
+$horario = "null";
+$puesto = "null";
 $usuario = "null";
 
 //generamos la consulta
-$sql = "SELECT PER.contrasena,
-concat (CUP.fk_sede, CUP.cupo) as cupo,
-USU.id_usuario as usuario
+$sql = "SELECT PER.id_personal,
+CUP.fk_sede as sede,
+CUP.cupo as cupo,
+PER.nombre_personal,
+PER.apellido_p,
+PER.apellido_m,
+TEL.fk_lada as lada,
+TEL.telefono,
+PER.contrasena,
+HOR.hr_nombre as horario,
+PUE.nombre_puesto as puesto,
+USU.usuario
 FROM personal PER, telefonos TEL, cupos CUP, horarios HOR, puestos PUE,usuarios USU
 WHERE PER.fk_cupo = CUP.id_cupo and
 PER.fk_telefono = TEL.id_telefono and
@@ -32,12 +52,20 @@ if(!$result = mysqli_query($conexion, $sql)) die();
 
 while($row = mysqli_fetch_array($result)) { 
     $contrasena_db=$row['contrasena'];
-    $cupo=$row['cupo'];
-    $usuario=$row['usuario'];
+    $id_personal = $row['id_personal'];
+$sede = $row['sede'];
+$cupo = $row['cupo'];
+$nombre_personal = $row['nombre_personal'];
+$apellido_p = $row['apellido_p'];
+$apellido_m = $row['apellido_m'];
+$lada = $row['lada'];
+$telefono = $row['telefono'];
+$contrasena = $row['contrasena'];
+$horario = $row['horario'];
+$puesto = $row['puesto'];
+$usuario = $row['usuario'];
 }
-
-
-    
+   
 //desconectamos la base de datos
 $close = mysqli_close($conexion) 
 or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
@@ -45,7 +73,18 @@ or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
 if($usuario == "1"){
     if($contrasena_db == $contrasena_ing){
         session_start();
-        $_SESSION["cupo"] = $cupo;
+$_SESSION['id_personal_admin'] = $id_personal;
+$_SESSION['sede_admin'] = $sede;
+$_SESSION['cupo_admin'] = $cupo;
+$_SESSION['nombre_personal_admin'] = $nombre_personal;
+$_SESSION['apellido_p_admin_admin'] = $apellido_p;
+$_SESSION['apellido_m_admin'] = $apellido_m;
+$_SESSION['lada_admin'] = $lada;
+$_SESSION['telefono_admin'] = $telefono;
+$_SESSION['contrasena_admin'] = $contrasena;
+$_SESSION['horario_admin'] = $horario;
+$_SESSION['puesto_admin'] = $puesto;
+$_SESSION['usuario_admin'] = $usuario;
         header("location:root.php");
     }else{
         echo "contrasenia o telefono incorrecto";
@@ -53,7 +92,18 @@ if($usuario == "1"){
 }else if($usuario == "2"){
     if($contrasena_db == $contrasena_ing){
         session_start();
-        $_SESSION["cupo_admin"] = $cupo;
+$_SESSION['id_personal_admin'] = $id_personal;
+$_SESSION['sede_admin'] = $sede;
+$_SESSION['cupo_admin'] = $cupo;
+$_SESSION['nombre_personal_admin'] = $nombre_personal;
+$_SESSION['apellido_p_admin_admin'] = $apellido_p;
+$_SESSION['apellido_m_admin'] = $apellido_m;
+$_SESSION['lada_admin'] = $lada;
+$_SESSION['telefono_admin'] = $telefono;
+$_SESSION['contrasena_admin'] = $contrasena;
+$_SESSION['horario_admin'] = $horario;
+$_SESSION['puesto_admin'] = $puesto;
+$_SESSION['usuario_admin'] = $usuario;
         header("location:admin.php");
     }else{
         echo "contrasenia o telefono incorrecto";
@@ -61,7 +111,18 @@ if($usuario == "1"){
 }else if($usuario == "3"){
     if($contrasena_db == $contrasena_ing){
         session_start();
-        $_SESSION["cupo_user"] = $cupo;
+$_SESSION['id_personal_user'] = $id_personal;
+$_SESSION['sede_user'] = $sede;
+$_SESSION['cupo_user'] = $cupo;
+$_SESSION['nombre_personal_user'] = $nombre_personal;
+$_SESSION['apellido_p_admin_user'] = $apellido_p;
+$_SESSION['apellido_m_user'] = $apellido_m;
+$_SESSION['lada_user'] = $lada;
+$_SESSION['telefono_user'] = $telefono;
+$_SESSION['contrasena_user'] = $contrasena;
+$_SESSION['horario_user'] = $horario;
+$_SESSION['puesto_user'] = $puesto;
+$_SESSION['usuario_user'] = $usuario;
         header("location:user.php");
     }else{
         echo "contrasenia o telefono incorrecto";
@@ -69,7 +130,18 @@ if($usuario == "1"){
 }else if($usuario == "4"){
     if($contrasena_db == $contrasena_ing){
         session_start();
-        $_SESSION["cupo_regis"] = $cupo;
+$_SESSION['id_personal_regis'] = $id_personal;
+$_SESSION['sede_regis'] = $sede;
+$_SESSION['cupo_regis'] = $cupo;
+$_SESSION['nombre_personal_regis'] = $nombre_personal;
+$_SESSION['apellido_p_admin_regis'] = $apellido_p;
+$_SESSION['apellido_m_regis'] = $apellido_m;
+$_SESSION['lada_regis'] = $lada;
+$_SESSION['telefono_regis'] = $telefono;
+$_SESSION['contrasena_regis'] = $contrasena;
+$_SESSION['horario_regis'] = $horario;
+$_SESSION['puesto_regis'] = $puesto;
+$_SESSION['usuario_regis'] = $usuario;
         header("location:regis.php");
     }else{
         echo "contrasenia o telefono incorrecto";
