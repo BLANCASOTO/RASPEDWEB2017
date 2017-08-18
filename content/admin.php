587aae482180ca94b806685b39d4fc5a9d4a8826
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-18 09:03:26 -0500

    Update admin.php

--- content/admin.php
+++ content/admin.php
@@ -89,23 +89,32 @@ echo $cupo;
 
 <div class="tab-content">
   <div id="home" class="tab-pane fade in active">
-    <table>
-<tbody>
-  <!-- Aplicadas en las filas -->
-  <tr class="active">...</tr>
-  <tr class="success">...</tr>
-  <tr class="warning">...</tr>
-  <tr class="danger">...</tr>
- 
-  <!-- Aplicadas en las celdas (<td> o <th>) -->
-  <tr>
-    <td class="active">...</td>
-    <td class="success">...</td>
-    <td class="warning">...</td>
-    <td class="danger">...</td>
-  </tr>
-</tbody>
-</table>
+    <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
   </div>
   <div id="menu1" class="tab-pane fade">
     <h3>Menu 1</h3>
