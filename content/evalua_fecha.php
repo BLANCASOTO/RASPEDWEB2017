b0c008f038af2502c3865ee779c07f6c1e622342
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-22 15:26:11 -0500

    Update evalua_fecha.php

--- content/evalua_fecha.php
+++ content/evalua_fecha.php
@@ -2,27 +2,12 @@
 session_start();
 require_once ('mysql-login.php');
 //Creamos la conexión
+
 $conexion = mysqli_connect($server, $user, $pass,$bd) or die("Ha sucedido un error inexperado en la conexion de la base de datos");
 mysqli_set_charset($conexion, "utf8"); 
 
-<!--
-$fecha = $_SESSION['fecha'];
-$id_personal_regis = $_SESSION['id_personal_regis'];
-
-
-$fecha_actual = strtotime(date("d-m-Y H:i:00",)); 
- $fecha = strtotime("dd-mm-yyyy"); 
- if($fecha_actual >  
- $fecha_entrada)
- 
- {
- echo "La fecha entrada ya ha pasado"; 
- } 
- else
- {
- echo "Aun falta algun tiempo";
- }
- -->
+ echo $fecha." es menor o igual a ".date(j);
+?>
  
  
  
